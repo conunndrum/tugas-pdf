d7b0c28adab51ad12cffa6eb78f69d989506165e
conunndrum <ayubii2709@gmail.com>
2025-05-20 08:56:34 +0700

    Update domz.js

--- domz.js
+++ domz.js
@@ -1,31 +1,18 @@
-const taskInput = document.getElementById('taskInput');
-const taskList = document.getElementById('taskList');
-
-// Muat tugas dari localStorage saat halaman dimuat
-window.onload = function () {
-const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-savedTasks.forEach(text => createTaskItem(text));
-};
-
-function addTask() {
-const text = taskInput.value.trim();
-if (text === "") {
-    alert("Input tidak boleh kosong!");
-    return;
-}
-createTaskItem(text);
-saveToLocalStorage();
-taskInput.value = "";
+function tambah() {
+let id = document.getElementById("ayub").value;
+let li = document.createElement("li");
+li.innerText = id;
+li.id = "hapusSaya";
+document.getElementById("container").appendChild(li);
 }
 
-function createTaskItem(text) {
-const li = document.createElement('li');
-li.textContent = text;}
-
-const deleteBtn = document.createElement('button');
-deleteBtn.textContent = 'Hapus';
-deleteBtn.onclick = function () {
-    taskList.removeChild(li);
-    saveToLocalStorage();
-};
+function hapus() {
+    let container = document.getElementById("container");
+    let target = document.getElementById("hapusSaya");
+    if (target){
+        container.removeChild(target);
+    } else{
+        alert("list belum ada atau sudah di hapus");
+    }
+}
 
